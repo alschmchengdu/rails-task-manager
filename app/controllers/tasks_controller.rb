f4c1e06847744dc1040a4ea5400156fe02f22824
Alison Schmidt <alison.e.schmidt@gmail.com>
2018-05-08 16:25:45 +0800

    Still need to add styling and workout a few kinks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,32 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:title],
+                    description: params[:description],
+                    completed: params[:completed])
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #building a new route to list the tasks
+  get '/tasks', to: 'tasks#index'
+
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks',     to: 'tasks#create', as: :create_task
+
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: "tasks#edit", as: :edit_task
+  patch "tasks/:id",    to: "tasks#update", as: :update_task
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
+
+
+
 end
+
+
+# Rails.application.routes.draw do
+#   get    "restaurants",          to: "restaurants#index"
+
+#   get    "restaurants/new",      to: "restaurants#new"
+#   post   "restaurants",          to: "restaurants#create"
+
+#   # NB: The `show` route needs to be *after* `new` route.
+#   get    "restaurants/:id",      to: "restaurants#show"
+
+#   get    "restaurants/:id/edit", to: "restaurants#edit"
+#   patch  "restaurants/:id",      to: "restaurants#update"
+
+#   delete "restaurants/:id",      to: "restaurants#destroy"
+# end
